a0d78dba487182408af1b949c9fc30592cd17b75
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2024-06-23 18:44:39 +0600

    created navbar

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,15 +1,49 @@
-import { onAuthStateChanged } from "firebase/auth";
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import { createContext, useState } from "react";
 import auth from "../firebase.config";
-
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
 export const AuthContext = createContext(null);
 
+// eslint-disable-next-line react/prop-types
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+
+  const signUpWithEmailAndPassword = (email, passworrd) => {
+    return createUserWithEmailAndPassword(auth, email, passworrd);
+  };
+
+  const signInWithGoogle = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const signInWithGithub = () => {
+    return signInWithPopup(auth, githubProvider);
+  };
+
+  const signOutUser = () => {
+    return signOut(auth);
+  };
+
   onAuthStateChanged(auth, (user) => {
     setUser(user);
   });
-  const authInfo = { user };
+
+  const authInfo = {
+    user,
+    signUpWithEmailAndPassword,
+    signInWithGoogle,
+    signInWithGithub,
+    signOutUser,
+  };
+
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,90 @@
-import { useContext } from "react";
-import AuthProvider from "../Providers/AuthProvider";
+import { Link, NavLink } from "react-router-dom";
 
 const Nav = () => {
-  const { user } = useContext(AuthProvider);
-  console.log(user);
-  return <div>Nav</div>;
+  // const { user } = useContext(AuthProvider);
+  // console.log(user);
+
+  const navLinks = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      {/* <li>
+        <details>
+          <summary>Parent</summary>
+          <ul className="p-2">
+            <li>
+              <a>Submenu 1</a>
+            </li>
+            <li>
+              <a>Submenu 2</a>
+            </li>
+          </ul>
+        </details>
+      </li> */}
+      <li>
+        <NavLink to="/all-arts">All Arts</NavLink>
+      </li>
+
+      <li>
+        <NavLink to="/add-craft">Add Craft</NavLink>
+      </li>
+
+      <li>
+        <NavLink to="/my-crafts">My Crafts</NavLink>
+      </li>
+
+      <li>
+        <NavLink to="/signup">SignUp</NavLink>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn pl-0 btn-ghost lg:hidden"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] space-y-4 mt-3 w-52 p-2 shadow"
+          >
+            {navLinks}
+          </ul>
+        </div>
+        <Link to="/" className="btn btn-ghost text-xl">
+          Drawing
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navLinks}</ul>
+      </div>
+      <div className="navbar-end">
+        <NavLink to="/login" className="btn">
+          Login
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
