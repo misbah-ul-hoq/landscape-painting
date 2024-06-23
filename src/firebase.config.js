3efa269d9bb472ff96568cc3862abb4141210ee1
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2024-06-23 19:40:45 +0600

    env variables workded

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB97Fw0zi2pMJ0xm_LKb2DPgPiN9rNNsSI",
-  authDomain: "fir-caaac.firebaseapp.com",
-  projectId: "fir-caaac",
-  storageBucket: "fir-caaac.appspot.com",
-  messagingSenderId: "817216405766",
-  appId: "1:817216405766:web:62c6928d9c8f0899569e87",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
